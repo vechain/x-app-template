63e549798fa58591e2f36848d5be72d3a3a5ef44
Piero Bassa <pierobassa222@gmail.com>
2024-05-28 09:29:23 +0200

    feat(utils): picasso

---
+++ packages/utils/PicassoUtils/PicassoUtils.spec.ts
@@ -0,0 +1,14 @@
+import { getPicassoImgSrc } from "./PicassoUtils"
+import { describe, expect } from "vitest"
+
+describe("getPicassoImgSrc", () => {
+  test('returns a base64 encoded string when "base64" argument is true', () => {
+    const result = getPicassoImgSrc("0x5f5e", true)
+    expect(result.startsWith("data:image/svg+xml;base64,")).toBe(true)
+  })
+
+  test('returns a non-base64 encoded string when "base64" argument is false', () => {
+    const result = getPicassoImgSrc("0x5f5e")
+    expect(result.startsWith("data:image/svg+xml;utf8,")).toBe(true)
+  })
+})
---
+++ packages/utils/PicassoUtils/PicassoUtils.ts
@@ -0,0 +1,10 @@
+import picasso from "@vechain/picasso"
+
+export const getPicassoImgSrc = (address: string, base64 = false) => {
+    const image = picasso(address.toLowerCase())
+    if (base64) {
+        const base64data = Buffer.from(image, "utf8").toString("base64")
+        return `data:image/svg+xml;base64,${base64data}`
+    }
+    return `data:image/svg+xml;utf8,${image}`
+}
