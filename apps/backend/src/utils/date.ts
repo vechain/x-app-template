1b0696d1929a2943372d32192c0f973fd0088190
Piero Bassa <pierobassa222@gmail.com>
2024-05-28 09:14:47 +0200

    feat(BE): date utils

---
+++ apps/backend/src/utils/date.ts
@@ -0,0 +1,7 @@
+export const getTimestampTodayTomorrow = () => {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
+  const tomorrow = new Date(today);
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  return { today, tomorrow };
+};
