798868ba60d49fd40993866d3dd5e3d8f46edcdd
Piero Bassa <pierobassa222@gmail.com>
2024-05-28 09:18:05 +0200

    feat(BE): server init

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,12 @@
+import { App } from '@/app';
+import { ValidateEnv } from '@utils/validateEnv';
+import { initializeOpenAI } from './utils/initializeOpenAI';
+import { SubmissionRoute } from './routes/submission.route'; 
+
+ValidateEnv();
+
+export const openAIHelper = initializeOpenAI();
+
+const app = new App([new SubmissionRoute()]);
+
+app.listen();
