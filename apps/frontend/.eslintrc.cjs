4673d589eda438a50af47cbd3f9d74573a32bf66
Piero Bassa <pierobassa222@gmail.com>
2024-05-28 09:28:12 +0200

    chore(FE): config files

--- apps/frontend/.eslintrc.cjs
+++ apps/frontend/.eslintrc.cjs
@@ -1,5 +1,18 @@
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["@repo/eslint-config/index.js"],
-};
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
