f189c4a982989f6fb9b787abe33bca685ee72c6f
Piero Bassa <pierobassa222@gmail.com>
2024-05-28 09:27:09 +0200

    feat(FE): main App component

---
+++ apps/frontend/src/App.tsx
@@ -0,0 +1,57 @@
+import { DAppKitProvider } from "@vechain/dapp-kit-react";
+import { ChakraProvider, Container, Flex } from "@chakra-ui/react";
+import {
+  Dropzone,
+  Footer,
+  InfoCard,
+  Instructions,
+  Navbar,
+  SubmissionModal,
+} from "./components";
+import { lightTheme } from "./theme";
+import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
+
+// RECaptcha V3 site key (https://developers.google.com/recaptcha/docs/v3)
+const VITE_RECAPTCHA_V3_SITE_KEY = import.meta.env
+  .VITE_RECAPTCHA_V3_SITE_KEY as string;
+
+function App() {
+  console.log(VITE_RECAPTCHA_V3_SITE_KEY);
+  return (
+    <GoogleReCaptchaProvider reCaptchaKey={VITE_RECAPTCHA_V3_SITE_KEY}>
+      <ChakraProvider theme={lightTheme}>
+        <DAppKitProvider
+          usePersistence
+          requireCertificate={false}
+          genesis="test"
+          nodeUrl="https://testnet.vechain.org/"
+          logLevel={"DEBUG"}
+        >
+          <Navbar />
+          <Flex flex={1}>
+            <Container
+              mt={{ base: 4, md: 10 }}
+              maxW={"container.xl"}
+              mb={{ base: 4, md: 10 }}
+              display={"flex"}
+              flex={1}
+              alignItems={"center"}
+              justifyContent={"flex-start"}
+              flexDirection={"column"}
+            >
+              <InfoCard />
+              <Instructions />
+              <Dropzone />
+            </Container>
+          </Flex>
+          <Footer />
+
+          {/* MODALS  */}
+          <SubmissionModal />
+        </DAppKitProvider>
+      </ChakraProvider>
+    </GoogleReCaptchaProvider>
+  );
+}
+
+export default App;
