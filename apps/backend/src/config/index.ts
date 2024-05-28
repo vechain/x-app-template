2e697cf6a1352eb6681ce76efaa6bdfdc2f47189
Piero Bassa <pierobassa222@gmail.com>
2024-05-28 09:09:07 +0200

    feat(BE): config

---
+++ apps/backend/src/config/index.ts
@@ -0,0 +1,16 @@
+import { config } from "dotenv";
+import { mnemonic } from "thor-devkit";
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+export const CREDENTIALS = process.env.CREDENTIALS === "true";
+export const { NODE_ENV, PORT, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const { OPENAI_API_KEY } = process.env;
+export const { MAX_FILE_SIZE } = process.env;
+export const { ADMIN_MNEMONIC, ADMIN_ADDRESS } = process.env;
+export const { NETWORK_URL, NETWORK_TYPE } = process.env;
+export const { RECAPTCHA_SECRET_KEY } = process.env;
+export const { REWARD_AMOUNT } = process.env;
+
+export const ADMIN_PRIVATE_KEY = mnemonic.derivePrivateKey(
+  ADMIN_MNEMONIC.split(" ")
+);
